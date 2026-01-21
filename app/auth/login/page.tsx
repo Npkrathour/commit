36e664efaa2a30c2897b769d075a858efbc03a34
npkrathour <pinkurathour.dev@gmail.com>
2026-01-22 02:46:30 +0530

    create home page

---
+++ app/auth/login/page.tsx
@@ -0,0 +1,9 @@
+import { Card } from "@/components/ui/card";
+
+const login = () => {
+  return (
+    <div className="w-full max-w-md mx-auto p-6 h-screen flex flex-col justify-center border border-gray-300"></div>
+  );
+};
+
+export default login;
