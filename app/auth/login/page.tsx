e98fb38a654670945a9d02e16e4b1817123c48b0
npkrathour <pinkurathour.dev@gmail.com>
2026-01-27 23:23:19 +0530

    fix code

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -2,7 +2,9 @@ import { Card } from "@/components/ui/card";
 
 const login = () => {
   return (
-    <div className="w-full max-w-md mx-auto p-6 h-screen flex flex-col justify-center border border-gray-300"></div>
+    <div className="w-full max-w-md mx-auto p-6 h-screen flex flex-col justify-center border border-gray-300">
+      <h1>Login Page is Here</h1>
+    </div>
   );
 };
 
