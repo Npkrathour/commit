918039953a5cb8a848bf4e186a26604b16ba9277
npkrathour <pinkurathour.dev@gmail.com>
2026-02-08 21:59:49 +0530

    add login auth

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,15 +1,3 @@
-// import axios from "axios";
-
-// const axiosInstance = axios.create({
-//   baseURL: "https://dummyjson.com",
-//   headers: {
-//     "Content-Type": "application/json",
-//   },
-//   timeout: 5000,
-// });
-
-// export default axiosInstance;
-
 import axios from "axios";
 
 const axiosInstance = axios.create({
